5f90ec6612bc59b35428e27ea58a496bd6530902
Pedro Vinicius <pedrovinicius096@gmail.com>
2026-05-04 17:58:30 -0300

    Implementando rotas CRUD

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,64 @@
-from fastapi import FastAPI
+from http import HTTPStatus
 
-app = FastAPI()
+from fastapi import FastAPI, HTTPException
+
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+
+app = FastAPI(title='Minha API Pika')
 
 
-@app.get('/')
+database = []
+
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+# @app.get('/html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+# def read_html():
+#     return """
+#     <html>
+#         <head>
+#             <title>Minha API Pika</title>
+#         </head>
+#         <body>
+#             <h1>Olá Mundo!</h1>
+#         </body>
+#     </html>"""
+
+
+@app.post('/users', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    # Aqui precisamos criar um novo modelo que represente o banco
+    # Precisamos de um identificador para esse registro!
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found')
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found')
+
+    database.pop(user_id - 1)
+
+    return {'message': 'User deleted successfully'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_deve_retornar_ok_e_ola_mundo():
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     """
     Esse teste tem 3 etapas (AAA)
     - A: Arrange - Arranjo: onde preparamos o cenário para o teste
@@ -13,7 +9,7 @@ def test_root_deve_retornar_ok_e_ola_mundo():
     - A: Assert  - Garanta que A é A
     """
     # Arrange
-    client = TestClient(app)
+    # client = TestClient(app)
 
     # Act
     response = client.get('/')
@@ -21,3 +17,76 @@ def test_root_deve_retornar_ok_e_ola_mundo():
     # Assert
     assert response.json() == {'message': 'Olá Mundo!'}
     assert response.status_code == HTTPStatus.OK
+
+
+# def test_root_html_deve_retornar_ok_e_ola_mundo():
+#     """
+#     Esse teste tem 3 etapas (AAA)
+#     - A: Arrange - Arranjo: onde preparamos o cenário para o teste
+#     - A: Act     - Acao: onde executamos a ação que queremos testar
+#     - A: Assert  - Garanta que A é A
+#     """
+#     # Arrange
+#     client = TestClient(app)
+
+#     # Act
+#     response = client.get('/html')
+
+#     # Assert
+#     assert '<h1>Olá Mundo!</h1>' in response.text
+#     assert response.status_code == HTTPStatus.OK
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'alice',
+                'email': 'alice@example.com',
+                'id': 1,
+            },
+        ],
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'novasenha'
+        }
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted successfully'}
